1d493afeb86ada39477cb7dbffe5d2a2c9aee91a
arch olorin <79698593+archolorin@users.noreply.github.com>
2021-09-23 14:06:14 +0000

    doc coverage

--- utils/StatusWindow.py
+++ utils/StatusWindow.py
@@ -8,24 +8,32 @@ class StatWin:
     sg.theme('darkgrey1')
 
     def statGui():
-      ops = [
-            [sg.Frame('Operational Metrics', 
-            [[sg.Text('Status:'), sg.T(om.stat)], 
-            [sg.T('Current Orders:'), sg.T(om.opsNum)]])], 
-            [sg.Submit('Modify'), sg.B('Refresh')]]
-
-      #statLayout = [sg.Frame('Operating Status', layout = ops)]
+        ops = [[
+            sg.Frame('Operational Metrics',
+                     [[sg.Text('Status:'), sg.T(om.stat)],
+                      [sg.T('Current Orders:'),
+                       sg.T(om.opsNum)]])
+        ], [sg.Submit('Modify'), sg.B('Refresh')]]
         
-      layout = [ops]
+        ops2 = [[
+            sg.Frame('Operational Metrics',
+                     [[sg.Text('Status:'), sg.T(om.stat)],
+                      [sg.T('Current Orders:'),
+                       sg.T(om.opsNum)]])
+        ], [sg.Submit('Modify'), sg.B('Refresh')]]
 
-      window = sg.Window('AIGCC Operations', layout)
-      while True:
-          event, values = window.Read()
-          if event == sg.WIN_CLOSED or event == 'Exit':
-              break
-          if event == 'Refresh':
-              utils.var.cmdRef()
-              #time.sleep(1)
-          if event == 'Modify':
-              break
-      window.close()
+        #statLayout = [sg.Frame('Operating Status', layout = ops)]
+
+        layout = [ops]
+
+        window = sg.Window('AIGCC Operations', layout)
+        while True:
+            event, values = window.Read()
+            if event in (sg.WIN_CLOSED, 'Exit'):
+                break
+            if event == 'Refresh':
+                utils.var.cmdRef()
+                #time.sleep(1)
+            if event == 'Modify':
+                break
+        window.close()
